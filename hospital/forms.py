d4628496dfedd4efc1fe47b35ea0aa2ffab0b969
Isaias <idearcos@uninorte.edu.co>
2021-10-25 00:21:34 -0500

    ya estan las bases de datos y validaciones

--- hospital/forms.py
+++ hospital/forms.py
@@ -1,27 +1,34 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.fields.core import DateField, IntegerField, SelectField
-
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from hospital.models import User
 
 lista_medicos=['Medico 1','Medico 2','Medico 3']
 lista_horas=['8:00 AM - 9:00 AM', '9:00 AM - 10:00 AM','10:00 AM - 11:00 AM','11:00 AM - 12:00 PM','12:00 PM - 1:00 PM','1:00 PM - 2:00 PM','2:00 PM - 3:00 PM','3:00 PM - 4:00 PM','4:00 PM - 5:00 PM','5:00 PM - 6:00 PM']
 
 
 class RegisterForm(FlaskForm):
-    nombres = StringField(label='Nombres:')
-    apellidos = StringField(label='Apellidos:')
-    email = StringField(label='Correo electrónico:')
-    documento = StringField(label='Su número de documento:')
-    password1 = PasswordField(label='Contraseña:')
-    password2 = PasswordField(label='Confirme su contraseña:')
-    submit = SubmitField(label='CREAR CUENTA')
+
+    def validate_email(self, email_to_check):
+        user = User.query.filter_by(email=email_to_check.data).first()
+        if user:
+            raise ValidationError('El correo ya se encuentra registrado')
+
+    nombres=StringField(label='Nombres:',validators=[DataRequired()])
+    apellidos=StringField(label='Apellidos',validators=[DataRequired()])
+    email=StringField(label='Email:',validators=[Email(),DataRequired()])
+    documento=StringField(label='Numero de documento:',validators=[DataRequired(),Length(min=6,max=20)])
+    password1=PasswordField(label='Contraseña:',validators=[DataRequired(),Length(min=4)])
+    password2=PasswordField(label='Confirme su contrasena:',validators=[DataRequired(),EqualTo('password1')])
+    submit=SubmitField(label='CREAR CUENTA')
 
 class CitasForm(FlaskForm):
-    paciente=StringField(label='Nombre:')
+    paciente=StringField(label='Nombre:',validators=[DataRequired()])
     #medico=StringField(label='Medico:')
-    medico=SelectField('Escoja un medico',choices=lista_medicos)
-    fecha=StringField(label='Escoja el dia')
+    medico=SelectField('Escoja un medico',choices=lista_medicos, validators=[DataRequired()])
+    fecha=StringField(label='Escoja el dia',validators=[DataRequired()])
     #hora=StringField(label='Escoja la hora')
-    hora=SelectField('Escoja la hora',choices=lista_horas)
+    hora=SelectField('Escoja la hora',choices=lista_horas,validators=[DataRequired()])
     submit = SubmitField(label='Agendar cita')
     
