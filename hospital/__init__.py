4cb4aaa46897952c9bc19dcb0224e2ddc68acc62
Isaias <idearcos@uninorte.edu.co>
2021-10-25 22:45:06 -0500

    bcrypt

--- hospital/__init__.py
+++ hospital/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-
-
+from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hospital.db'
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
