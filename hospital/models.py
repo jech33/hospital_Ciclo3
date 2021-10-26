8ac97607f5a1ce2faeafc48d9bddf9bd2bb76e53
Isaias <idearcos@uninorte.edu.co>
2021-10-26 00:02:07 -0500

    no login aun

--- hospital/models.py
+++ hospital/models.py
@@ -1,11 +1,11 @@
 from enum import unique
-from hospital import db, login_manager
+from hospital import db ##, login_manager
 from hospital import bcrypt
 from flask_login import UserMixin
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+##@login_manager.user_loader
+##def load_user(user_id):
+##    return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
