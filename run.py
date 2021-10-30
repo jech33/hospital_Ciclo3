2ec8e6141afb595b81c9a284d615fb83af890afc
Isaias <idearcos@uninorte.edu.co>
2021-10-29 20:55:15 -0500

    todo listoo

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from hospital import app
 
 #Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
 
 #prueba2
